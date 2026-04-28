cf195defdad26aa714cf72c945af3b6222f0a1a5
Don Bennett <dpb@donbennett.org>
2026-04-27 20:54:12 -0700

    removed scripts to a different repo.

--- bin/make-btrfs-installation-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SOURCE="p10"
-TARGET="p8"
-sudo mount /dev/nvme0n1${SOURCE} /mnt/${SOURCE}
-sudo mount -o subvol=/,compress=zstd /dev/nvme0n1${TARGET} /mnt/${TARGET}
-sudo ./make-btrfs.clj /mnt/${SOURCE} /mnt/${TARGET} "ubuntu 2404btest"
--- bin/ssd-sync-changes-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-EXCLUDES="--exclude=.mypy_cache/ --exclude=.pytest_cache/ --exclude=__pycache__ --exclude=.cache --exclude=.dtrash --exclude=CacheStorage"
-rsync -av -n  ${EXCLUDES} /media/dpb/ssd-home/volumes/dpb/Dropbox $HOME
